5eb4d05f4cbb51bb9de0659e0e62212a51d3e643
Jesse Tane <jesse.tane@gmail.com>
2014-09-06 16:58:26 -0400

    update benchmarks

--- bench/index.js
+++ bench/index.js
@@ -1,9 +1,9 @@
-var fsdb = require('../');
 var queue = require('queue');
 var rimraf = require('rimraf');
+var mkfsdb = require('../');
 
-// working dir
-var root = '/tmp/fsdb';
+var root = '/tmp/fsdb-bench';
+var fsdb = mkfsdb({ root: root });
 
 // queue up benchmarks synchronously
 var q = queue({ concurrency: 1 });
@@ -14,14 +14,16 @@ q.push(benchSimple.bind(null, 5000));
 q.start();
 
 function benchSimple(cnt, cb) {
-  var db = fsdb({ root: root });
-  var people = db.collect('people');
 
   function write(cb) {
     var x = 0;
     var start = +new Date;
     for (var i=0; i<cnt; i++) {
-      people.create({ name: 'john', middle: 'q', last: 'public' }, onwrite)
+      var person = fsdb({ location: 'people' });
+      person.files['first.txt'] = 'john';
+      person.files['middle.txt'] = 'q';
+      person.files['last.txt'] = 'public';
+      person.create(onwrite);
     }
 
     function onwrite(err) {
@@ -37,11 +39,11 @@ function benchSimple(cnt, cb) {
 
   function read(cb) {
     var start = +new Date;
-    people.ls(function(err, models) {
+    var people = fsdb('people').read(function(err) {
       if (err) throw err;
       var end = +new Date;
       var elapsed = ((end - start) / 1000).toFixed(3);
-      console.log(cnt + ' models read in ' + elapsed + ' seconds');
+      console.log(people.folders.length + ' models read in ' + elapsed + ' seconds');
       if (q.length) console.log();
       cb();
     });
